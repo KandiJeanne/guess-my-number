fe6373ee41274c55cfbd8fd45f27c8f78f1e3f42
Kandi Williams <kandijeannedesign@gmail.com>
2021-09-07 13:27:31 -0500

    implement game over logic

--- script.js
+++ script.js
@@ -12,6 +12,8 @@
 const secretNumber = Math.trunc(Math.random()*20)+1;
 document.querySelector('.number').textContent = secretNumber;
 
+let score = 10;
+
 document.querySelector('.check').addEventListener('click', function() {
     const guess = Number(document.querySelector('.guess').value);
 
@@ -20,9 +22,24 @@ document.querySelector('.check').addEventListener('click', function() {
     } else if (guess === secretNumber){
         document.querySelector('.message').textContent = 'Correct Number!';
     } else if (guess > secretNumber) {
-        document.querySelector('.message').textContent = "Too high!"
+        if (score > 1) {
+          document.querySelector('.message').textContent = "Too high!";
+          score --;
+          document.querySelector('.score').textContent = score;    
+        } else {
+            document.querySelector('.message').textContent = 'You lost the game!';
+            document.querySelector('.score').textContent = 0;
+        }
+        
     } else if (guess < secretNumber) {
-        document.querySelector('.message').textContent = "Too low!"
+        if (score > 1) {
+        document.querySelector('.message').textContent = "Too low!";
+        score --;
+        document.querySelector('.score').textContent = score;
+        } else {
+            document.querySelector('.message').textContent = 'You lost the game!';
+            document.querySelector('.score').textContent = 0;
+        }
     }
 })
 
