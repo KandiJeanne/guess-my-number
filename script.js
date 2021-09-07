518352b77d41352e14f88898891094cbf5221a22
Kandi Williams <kandijeannedesign@gmail.com>
2021-09-07 12:57:30 -0500

    implement high-low game logic

--- script.js
+++ script.js
@@ -9,10 +9,20 @@
 // document.querySelector('.guess').value = 23;
 // console.log( document.querySelector('.guess').value);
 
+const secretNumber = Math.trunc(Math.random()*20)+1;
+document.querySelector('.number').textContent = secretNumber;
+
 document.querySelector('.check').addEventListener('click', function() {
     const guess = Number(document.querySelector('.guess').value);
 
     if (!guess) {
       document.querySelector('.message').textContent = 'Please enter a number!'      
+    } else if (guess === secretNumber){
+        document.querySelector('.message').textContent = 'Correct Number!';
+    } else if (guess > secretNumber) {
+        document.querySelector('.message').textContent = "Too high!"
+    } else if (guess < secretNumber) {
+        document.querySelector('.message').textContent = "Too low!"
     }
 })
+
